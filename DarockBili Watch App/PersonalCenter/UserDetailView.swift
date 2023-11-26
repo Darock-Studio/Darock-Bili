fe297a558c63035a3d6a3694772afedc34f61cdc
WindowsMEMZ <45706356+WindowsMEMZ@users.noreply.github.com>
2023-11-26 22:05:10 +0800

    Update UserDetailView.swift

--- DarockBili Watch App/PersonalCenter/UserDetailView.swift
+++ DarockBili Watch App/PersonalCenter/UserDetailView.swift
@@ -4,6 +4,16 @@
 //
 //  Created by WindowsMEMZ on 2023/7/1.
 //
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the MeowBili open source project
+//
+// Copyright (c) 2023 Darock Studio and the MeowBili project authors
+// Licensed under GNU General Public License v3
+//
+// See https://darock.top/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
 
 // UserDetailView 对于 watchOS 10 和其他版本是两套独立代码，更改时记得同时更改！
 
