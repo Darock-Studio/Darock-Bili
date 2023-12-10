21ef8d94e0e0e72892177878ad1a8d9048a08833
Mark Chan <45706356+WindowsMEMZ@users.noreply.github.com>
2023-12-11 02:26:17 +0800

    Update DarockBili_Watch_App_UI_Tests.swift

--- DarockBili Watch App UI Tests/DarockBili_Watch_App_UI_Tests.swift
+++ DarockBili Watch App UI Tests/DarockBili_Watch_App_UI_Tests.swift
@@ -60,11 +60,11 @@ final class DarockBili_Watch_App_UI_Tests: XCTestCase {
         takeScreenshot(of: app, named: "NetworkFix1")
         sleep(5)
         takeScreenshot(of: app, named: "NetworkFix2")
+        
     }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
