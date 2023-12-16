2f4f8f63a99700127b73636f9434c70ab4539f80
Mark Chan <45706356+WindowsMEMZ@users.noreply.github.com>
2023-12-16 17:03:49 +0800

    Update AboutView.swift

--- DarockBili Watch App/Others/AboutView.swift
+++ DarockBili Watch App/Others/AboutView.swift
@@ -19,11 +19,19 @@ import SwiftUI
 import DarockKit
 
 struct AboutView: View {
+     @State var isUiTestViewPresented = false
      var body: some View {
          NavigationStack {
             List {
                 Section {
-                    Text("喵哩喵哩 v\(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String) Build \(Bundle.main.infoDictionary?["CFBundleVersion"] as! String)")
+                    VStack {
+                        NavigationLink("", isActive: $isUiTestViewPresented, destination: {DebugUITestView()})
+                            .frame(width: 0, height: 0)
+                        Text("喵哩喵哩 v\(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String) Build \(Bundle.main.infoDictionary?["CFBundleVersion"] as! String)")
+                            .onTapGesture(count: 10) {
+                                isUiTestViewPresented = true
+                            }
+                    }
                     Text("编译时间: \(CodingTime.getCodingTime())")
                     Text("遇到问题？在设置页面点击“反馈问题”进行反馈，感谢您的支持！")
                 }
@@ -32,6 +40,7 @@ struct AboutView: View {
                     Text("Darock Studio")
                     Label("来自 \(Text("Darock Studio").bold()) 的消息：欢迎加群 248036605 获取最新消息谢谢喵！", systemImage: "arrowshape.up")
                     Text("ThreeManager785")
+                    Text("-- You --")
                 }
                 Section {
                     NavigationLink(destination: {OpenSource()}, label: {
@@ -98,6 +107,16 @@ struct AboutView: View {
             }
         }
     }
+}
+
+struct DebugUITestView: View {
+    var body: some View {
+        List {
+            NavigationLink(destination: {OpenSource()}, label: {
+                Text("LongUIDUserTest")
+            })
+        }
+   }
 }
 
 struct AboutView_Previews: PreviewProvider {
