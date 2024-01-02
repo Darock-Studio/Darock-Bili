9b7483b61032473c0424edbb83c10266c850fd3a
Mark Chan <45706356+WindowsMEMZ@users.noreply.github.com>
2024-01-03 00:03:15 +0800

    Update AboutView.swift (#52)
    * Update AboutView.swift
    
    * Update DarockBili_Watch_App_UI_Tests.swift

--- DarockBili Watch App/Others/AboutView.swift
+++ DarockBili Watch App/Others/AboutView.swift
@@ -27,11 +27,15 @@ struct AboutView: View {
                     Text("编译时间: \(CodingTime.getCodingTime())")
                     Text("遇到问题？在设置页面点击“反馈问题”进行反馈，感谢您的支持！")
                 }
-                Section {
-                    Text("Credits:")
-                    Text("Darock Studio")
-                    Label("来自 \(Text("Darock Studio").bold()) 的消息：欢迎加群 248036605 获取最新消息谢谢喵！", systemImage: "arrowshape.up")
+                Section(header: Text("Credits")) {
+                    VStack {
+                        Text("Darock Studio")
+                        Label("来自 \(Text("Darock Studio").bold()) 的消息：欢迎加群 248036605 获取最新消息谢谢喵！", systemImage: "arrowshape.up")
+                    }
                     Text("ThreeManager785")
+                    Text("Lightning-Lion")
+                    Text("Linecom")
+                    Text("-- And You --")
                 }
                 Section {
                     NavigationLink(destination: {OpenSource()}, label: {
