3467b4483269adab7a3773c3de5acff859e49d30
WindowsMEMZ <wmemz2333@163.com>
2024-01-05 19:45:30 +0800

    Trying to fix issue

--- DarockBili Watch App/PersonalCenter/UserDetailView.swift
+++ DarockBili Watch App/PersonalCenter/UserDetailView.swift
@@ -564,8 +564,8 @@ struct UserDetailView: View {
         func RefreshVideos() {
             videos = [[String: String]]()
             let headers: HTTPHeaders = [
-                "accept-language": "en,zh-CN;q=0.9,zh;q=0.8",
-                "cookie": "SESSDATA=\(sessdata); buvid3=\(globalBuvid3); buvid4=\(globalBuvid4);",
+                //"accept-language": "en,zh-CN;q=0.9,zh;q=0.8",
+                "cookie": "SESSDATA=\(sessdata);", // buvid3=\(globalBuvid3); buvid4=\(globalBuvid4);
                 "User-Agent": "Mozilla/5.0" // Bypass? drdar://gh/SocialSisterYi/bilibili-API-collect/issues/868/1859065874
             ]
             // TODO: Check Wbi Sign ability when encoding array args.
