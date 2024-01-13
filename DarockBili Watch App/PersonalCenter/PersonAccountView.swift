3f255e4098c38331c004ebe214554e67175f0298
WindowsMEMZ <wmemz2333@163.com>
2024-01-13 23:05:25 +0800

    Fix build issue

--- DarockBili Watch App/PersonalCenter/PersonAccountView.swift
+++ DarockBili Watch App/PersonalCenter/PersonAccountView.swift
@@ -226,7 +226,7 @@ struct PersonAccountView: View {
                             if username == "" {
                                 getBuvid(url: "https://api.bilibili.com/x/space/wbi/acc/info".urlEncoded()) { buvid3, buvid4, _uuid, resp in
                                 let headers: HTTPHeaders = [
-                                    "cookie": "SESSDATA=\(sessdata); innersign=0; buvid3=\(buvid3); b_nut=1704873471; i-wanna-go-back=-1; b_ut=7; b_lsid=9910433CB_18CF260AB89; _uuid=\(_uuid); enable_web_push=DISABLE; header_theme_version=undefined; home_feed_column=4; browser_resolution=3440-1440; buvid4=\(buvid4); buvid_fp=\(BuvidFp.gen(key: "Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36", seed: 31))",
+                                    "cookie": "SESSDATA=\(sessdata); innersign=0; buvid3=\(buvid3); b_nut=1704873471; i-wanna-go-back=-1; b_ut=7; b_lsid=9910433CB_18CF260AB89; _uuid=\(_uuid); enable_web_push=DISABLE; header_theme_version=undefined; home_feed_column=4; browser_resolution=3440-1440; buvid4=\(buvid4); buvid_fp=\((try? BuvidFp.gen(key: "Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36", seed: 31)) ?? "")",
                                     "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
                                 ]
                                     biliWbiSign(paramEncoded: "mid=\(dedeUserID)".base64Encoded()) { signed in
