7546a9c0d96592d58f3be8e0bb8de71dd108f0f6
Mark Chan <45706356+WindowsMEMZ@users.noreply.github.com>
2024-01-17 23:17:44 +0800

    First watch only - Picked (#73)
    * Update ios.yml
    
    * Delete DarockBili Watch App UI Tests directory
    
    * Delete DarockBili Watch App Unit Tests directory
    
    * Delete DarockBili directory
    
    * Create BangumiCommentsView.swift
    
    * Create BangumiDetailView.swift
    
    * Create BangumiDownloadView.swift
    
    * Create BangumiPlayerView.swift
    
    * Create LiveDetailView.swift
    
    * Update Localizable.xcstrings
    
    * Update project.pbxproj
    
    * Update Package.resolved
    
    * Delete DarockBili.xcodeproj/project.xcworkspace/xcuserdata directory
    
    * Update ad-hoc.plist

---
+++ DarockBili Watch App/Bangumi/BangumiDownloadView.swift
@@ -0,0 +1,18 @@
+//
+//  BangumiDownloadView.swift
+//  DarockBili Watch App
+//
+//  Created by 雷美淳 on 2024/1/13.
+//
+
+import SwiftUI
+
+struct BangumiDownloadView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    BangumiDownloadView()
+}
