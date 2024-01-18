75e7e8325484eed82e8eb3163c09ca7b269d1f2a
Mark Chan <45706356+WindowsMEMZ@users.noreply.github.com>
2024-01-18 23:06:36 +0800

    修复总体访问权限不足问题 (#67) (#77)
    * Opt - Personal account less net req
    
    * Added buvid_fp logic
    
    * Fix build issue
    
    * Add Test
    
    * Fix build issue
    
    * Trying fix issue
    
    * Added Buvid active logic & _uuid calculation logic
    
    * Fix build issue
    
    * Fix build issue & Added tests
    
    * Fix build issue
    
    * Fix build issue
    
    * Fix build issue
    
    * Update CodeExt.swift
    
    * Fix build issue
    
    * Fix build issue
    
    * Fix build issue
    
    * Apply logic to PersonAccountView
    
    * Updated tests
    
    * Fix build issue
    
    * Fix build issue
    
    * Added auto rerequest func
    
    * Fix build issue
    
    * Update CodeExt.swift
    
    * Update CodeExt.swift
    
    * Fast merge main into fix/mydetail-access (#74)
    
    * Update ssh-debug.yml
    
    * 添加用户等级显示 (#71)
    
    * Updated user VIP & Level show
    
    * Removed phone login
    
    * Fix build issue
    
    * Fix UI Spacer
    
    * 修复播放解析崩溃问题 (#69)
    
    * Update VideoDetailView.swift
    
    * Update VideoDetailView.swift
    
    * Fix build issue
    
    * Updated dec
    
    * Final apply
    
    * First watch only - Picked (#73)
    
    * Update ios.yml
    
    * Delete DarockBili Watch App UI Tests directory
    
    * Delete DarockBili Watch App Unit Tests directory
    
    * Delete DarockBili directory
    
    * Create BangumiCommentsView.swift
    
    * Create BangumiDetailView.swift
    
    * Create BangumiDownloadView.swift
    
    * Create BangumiPlayerView.swift
    
    * Create LiveDetailView.swift
    
    * Update Localizable.xcstrings
    
    * Update project.pbxproj
    
    * Update Package.resolved
    
    * Delete DarockBili.xcodeproj/project.xcworkspace/xcuserdata directory
    
    * Update ad-hoc.plist
    
    * Update and rename unit-test.yml to company-export.yml
    
    * Update statuscheck-runner.yml
    
    * Update UserDetailView.swift
    
    * Update company-export.yml
    
    * Update CodeExt.swift
    
    * Update DarockBiliApp.swift
    
    * Update AboutView.swift
    
    * Update CodeExt.swift
    
    * Update company-export.yml
    
    * Update CodeExt.swift
    
    * Update CodeExt.swift
    
    * Update CodeExt.swift
    
    * Update CodeExt.swift
    
    * Update MainView.swift
    
    * Update UserDetailView.swift
    
    * Update AboutView.swift
    
    * Update CodeExt.swift
    
    * Update AboutView.swift
    
    * Update SettingsView.swift
    
    * Update CodeExt.swift
    
    * Update NetworkFixView.swift
    
    * Update SettingsView.swift

--- DarockBili Watch App/PersonalCenter/UserDetailView.swift
+++ DarockBili Watch App/PersonalCenter/UserDetailView.swift
@@ -151,7 +151,7 @@ struct UserDetailView: View {
             biliWbiSign(paramEncoded: "mid=\(uid)".base64Encoded()) { signed in
                 if let signed {
                     debugPrint(signed)
-                    DarockKit.Network.shared.requestJSON("https://api.bilibili.com/x/space/wbi/acc/info?\(signed)", headers: headers) { respJson, isSuccess in
+                    autoRetryRequestApi("https://api.bilibili.com/x/space/wbi/acc/info?\(signed)", headers: headers) { respJson, isSuccess in
                         if isSuccess {
                             //debugPrint(respJson)
                             if !CheckBApiError(from: respJson) { return }
@@ -584,16 +584,17 @@ struct UserDetailView: View {
                 //"accept-encoding": "gzip, deflate, br",
                 //"accept-language": "zh-CN,zh;q=0.9",
                 //"cookie": "\(sessdata == "" ? "" : "SESSDATA=\(sessdata); ")buvid3=\(globalBuvid3); b_nut=\(Date.now.timeStamp); buvid4=\(globalBuvid4);", 
-                "cookie": "SESSDATA=\(sessdata);"
+                "cookie": "SESSDATA=\(sessdata); buvid_fp=e651c1a382430ea93631e09474e0b395",
                 //"origin": "https://space.bilibili.com",
                 //"referer": "https://space.bilibili.com/\(uid)/video",
                 //"User-Agent": "Mozilla/5.0" // Bypass? drdar://gh/SocialSisterYi/bilibili-API-collect/issues/868/1859065874
+                "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebK…KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
             ]
             // FIXME: Official Wbi crypto logic for this request seems different from other APIs, some IP can get but some can't. It's hard to fix ~_~
             biliWbiSign(paramEncoded: "mid=\(uid)&ps=50&pn=\(videoNowPage)".base64Encoded()) { signed in
                 if let signed {
                     debugPrint(signed)
-                    DarockKit.Network.shared.requestJSON("https://api.bilibili.com/x/space/wbi/arc/search?\(signed)", headers: headers) { respJson, isSuccess in
+                    autoRetryRequestApi("https://api.bilibili.com/x/space/wbi/arc/search?\(signed)", headers: headers) { respJson, isSuccess in
                         if isSuccess {
                             debugPrint(respJson)
                             if !CheckBApiError(from: respJson) { return } 
