abc14f64d0a6d8fd826cfa19f925ecc950b231a0
Mark Chan <45706356+WindowsMEMZ@users.noreply.github.com>
2024-01-18 22:05:06 +0800

    Update AboutView.swift

--- DarockBili Watch App/Others/AboutView.swift
+++ DarockBili Watch App/Others/AboutView.swift
@@ -36,7 +36,6 @@ struct AboutView: View {
                         Text("Darock Studio")
                         Label("来自 \(Text("Darock Studio").bold()) 的消息：欢迎加群 248036605 获取最新消息谢谢喵！", systemImage: "arrowshape.up")
                     }
-                    Text("ThreeManager785")
                     Text("Lightning-Lion")
                     Text("Linecom")
                     Text("-- And You --")
@@ -45,13 +44,6 @@ struct AboutView: View {
                     NavigationLink(destination: {OpenSource()}, label: {
                         Text("开源组件许可")
                     })
-                }
-                if debug {
-                    Section {
-                        NavigationLink(destination: {DebugMenuView()}, label: {
-                            Text("调试")
-                        })
-                    }
                 }
             }
             .bold()
@@ -110,87 +102,6 @@ struct AboutView: View {
                 Licensed under MIT license
                 ------------------
                 """)
-            }
-        }
-    }
-}
-
-struct DebugMenuView: View {
-    var body: some View {
-        List {
-            NavigationLink(destination: {UserDetailView(uid: "3546572635768935")}, label: {
-                Text("LongUIDUserTest")
-            })
-            NavigationLink(destination: {BuvidFpDebug()}, label: {
-                Text("buvid_fpTest")
-            })
-            NavigationLink(destination: {UuidDebug()}, label: {
-                Text("_uuid_Gen")
-            })
-            NavigationLink(destination: {Buvid34Debug()}, label: {
-                Text("buvid3_4_actived")
-            })
-        }
-    }
-
-    struct BuvidFpDebug: View {
-        @State var fp = ""
-        @State var resu = ""
-        var body: some View {
-            List {
-                TextField("fp", text: $fp)
-                Button(action: {
-                    do {
-                        resu = try BuvidFp.gen(key: fp, seed: 31)
-                    } catch {
-                        resu = "Failed: \(error)"
-                    }
-                }, label: {
-                    Text("Gen")
-                })
-                Text(resu)
-            }
-        }
-    }
-    struct UuidDebug: View {
-        @State var uuid = ""
-        var body: some View {
-            List {
-                Button(action: {
-                    uuid = UuidInfoc.gen()
-                }, label: {
-                    Text("Gen")
-                })
-                Text(uuid)
-            }
-        }
-    }
-    struct Buvid34Debug: View {
-        @State var activeBdUrl = "https://www.bilibili.com/"
-        @State var locBuvid3 = ""
-        @State var locBuvid4 = ""
-        @State var locUplResp = ""
-        var body: some View {
-            List {
-                Section {
-                    Text("Current Global Buvid3: \(globalBuvid3)")
-                    Text("Current Global Buvid4: \(globalBuvid4)")
-                }
-                Section {
-                    TextField("activeBdUrl", text: $activeBdUrl)
-                    Button(action: {
-                        getBuvid(url: activeBdUrl.urlEncoded()) { buvid3, buvid4, _, resp in
-                            locBuvid3 = buvid3
-                            locBuvid4 = buvid4
-                            locUplResp = resp
-                        }
-                    }, label: {
-                        Text("Get new & active")
-                    })
-                    Text(locBuvid3)
-                    Text(locBuvid4)
-                    Text(locUplResp)
-                }
             }
         }
     }
