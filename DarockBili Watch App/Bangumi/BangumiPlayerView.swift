07fd8fd4ff43a902e416bb3f6887efb28b7aa773
WindowsMEMZ <wmemz2333@163.com>
2024-01-24 16:50:10 +0800

    Add file copyright headers

--- DarockBili Watch App/Bangumi/BangumiPlayerView.swift
+++ DarockBili Watch App/Bangumi/BangumiPlayerView.swift
@@ -4,6 +4,16 @@
 //
 //  Created by 雷美淳 on 2024/1/13.
 //
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the MeowBili open source project
+//
+// Copyright (c) 2023 Darock Studio and the MeowBili project authors
+// Licensed under GNU General Public License v3
+//
+// See https://darock.top/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
 
 import SwiftUI
 
