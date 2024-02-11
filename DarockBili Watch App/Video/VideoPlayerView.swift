aa5a2d23537a7d6211cdffe619969cff1df1564a
WindowsMEMZ <wmemz2333@163.com>
2024-02-12 02:09:45 +0800

    Optimized iOS & watchOS player

--- DarockBili Watch App/Video/VideoPlayerView.swift
+++ DarockBili Watch App/Video/VideoPlayerView.swift
@@ -29,88 +29,96 @@ struct VideoPlayerView: View {
     @AppStorage("SESSDATA") var sessdata = ""
     @AppStorage("bili_jct") var biliJct = ""
     @AppStorage("RecordHistoryTime") var recordHistoryTime = "into"
-    @AppStorage("IsUseModifiedPlayer") var isUseModifiedPlayer = true
+    @AppStorage("IsDanmakuEnabled") var isDanmakuEnabled = true
     @AppStorage("IsVideoPlayerGestureEnabled") var isVideoPlayerGestureEnabled = true
     @State var currentTime: Double = 0.0
     @State var playerTimer: Timer?
+    @State var danmakuTimer: Timer?
     @State var showDanmakus = [[String: String]]()
-    @State var showedDanmakus: [[(danmaku: Int, offset: Double)]?] = [nil, nil, nil, nil]
     @State var tabviewChoseTab = 1
     @State var playerRotate = 0.0
     @State var player: AVPlayer!
     @State var danmakuOffset = 0.0
-    @State var lastDanmakuOffset = 0.0
-    @State var lastDanmakuLine = 0
-    @State var lastDanmakuIndex = 0
     var body: some View {
-//        let asset = AVURLAsset(url: URL(string: VideoDetailView.willPlayVideoLink)!/*, options: ["AVURLAssetHTTPHeaderFieldsKey": [
-//            "Referer": "https://www.bilibili.com/video/\(VideoDetailView.willPlayVideoBV)",
-//            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15",
-//            "platform": "html5"
-//                                                                                    ]]*/, options: [AVURLAssetHTTPUserAgentKey: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15"])
-//        let item = AVPlayerItem(asset: asset)
-//        let player = AVPlayer(playerItem: item)
-        
-        ZStack {
-            if #available(watchOS 10, *), isUseModifiedPlayer {
-                if player != nil {
-                    LSContentView(videoUrl: VideoDetailView.willPlayVideoLink, videoBvid: VideoDetailView.willPlayVideoBV, videoData: .init(enableFlyComment: true, currentCid: Int64(VideoDetailView.willPlayVideoCID) ?? 0), player: player)
-                }
-                
-            } else {
-                TabView(selection: $tabviewChoseTab) {
-                    ZStack {
-                        VideoPlayer(player: player)
-                            .rotationEffect(.degrees(playerRotate))
-                            .ignoresSafeArea()
-                            .onAppear {
-                                hideDigitalTime(true)
-                                Timer.scheduledTimer(withTimeInterval: 15, repeats: true) { timer in
-                                    playerTimer = timer
-                                    debugPrint(player.currentTime())
-                                    let headers: HTTPHeaders = [
-                                        "cookie": "SESSDATA=\(sessdata)"
-                                    ]
-                                    AF.request("https://api.bilibili.com/x/click-interface/web/heartbeat", method: .post, parameters: ["bvid": VideoDetailView.willPlayVideoBV, "mid": dedeUserID, "played_time": Int(player.currentTime().seconds), "type": 3, "dt": 2, "play_type": 0, "csrf": biliJct], headers: headers).response { response in
-                                        debugPrint(response)
-                                    }
-                                }
-                            }
-                            .onDisappear {
-                                hideDigitalTime(false)
-                                playerTimer?.invalidate()
-                            }
-                    }
-                    .tag(1)
-                    ScrollView {
-                        VStack {
-                            HStack {
-                                Button(action: {
-                                    if playerRotate - 90 > 0 {
-                                        playerRotate -= 90
-                                    } else {
-                                        playerRotate = 270
-                                    }
-                                }, label: {
-                                    Image(systemName: "rotate.left")
-                                })
-                                Button(action: {
-                                    if playerRotate + 90 < 360 {
-                                        playerRotate += 90
-                                    } else {
-                                        playerRotate = 0
-                                    }
-                                }, label: {
-                                    Image(systemName: "rotate.right")
-                                })
+        TabView(selection: $tabviewChoseTab) {
+            ZStack {
+                VideoPlayer(player: player)
+                    .rotationEffect(.degrees(playerRotate))
+                    .ignoresSafeArea()
+                    .onAppear {
+                        hideDigitalTime(true)
+                        Timer.scheduledTimer(withTimeInterval: 15, repeats: true) { timer in
+                            playerTimer = timer
+                            debugPrint(player.currentTime())
+                            let headers: HTTPHeaders = [
+                                "cookie": "SESSDATA=\(sessdata)"
+                            ]
+                            AF.request("https://api.bilibili.com/x/click-interface/web/heartbeat", method: .post, parameters: ["bvid": VideoDetailView.willPlayVideoBV, "mid": dedeUserID, "played_time": Int(player.currentTime().seconds), "type": 3, "dt": 2, "play_type": 0, "csrf": biliJct], headers: headers).response { response in
+                                debugPrint(response)
                             }
                         }
                     }
-                    .tag(2)
-                }
-                .tabViewStyle(.page)
+                    .onDisappear {
+                        hideDigitalTime(false)
+                    }
+                    .overlay {
+                        ZStack {
+                            if isDanmakuEnabled {
+                                VStack {
+                                    ForEach(0...3, id: \.self) { i in
+                                        ZStack {
+                                            ForEach(0..<showDanmakus.count, id: \.self) { j in
+                                                if j % 4 == i {
+                                                    if showDanmakus[j]["Type"]! == "1" || showDanmakus[j]["Type"]! == "2" || showDanmakus[j]["Type"]! == "3" {
+                                                        if Double(showDanmakus[j]["Appear"]!)! < player.currentTime().seconds + 10 && Double(showDanmakus[j]["Appear"]!)! + 10 > player.currentTime().seconds {
+                                                            Text(showDanmakus[j]["Text"]!)
+                                                                .font(.system(size: 14))
+                                                                .foregroundColor(Color(hex: Int(showDanmakus[j]["Color"]!)!))
+                                                                .offset(x: Double(showDanmakus[j]["Appear"]!)! * 50)
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    Spacer()
+                                }
+                                .allowsHitTesting(false)
+                                .offset(x: -danmakuOffset)
+                                .animation(.smooth, value: danmakuOffset)
+                            }
+                        }
+                    }
             }
+            .tag(1)
+            ScrollView {
+                VStack {
+                    HStack {
+                        Button(action: {
+                            if playerRotate - 90 > 0 {
+                                playerRotate -= 90
+                            } else {
+                                playerRotate = 270
+                            }
+                        }, label: {
+                            Image(systemName: "rotate.left")
+                        })
+                        Button(action: {
+                            if playerRotate + 90 < 360 {
+                                playerRotate += 90
+                            } else {
+                                playerRotate = 0
+                            }
+                        }, label: {
+                            Image(systemName: "rotate.right")
+                        })
+                    }
+                    Toggle(isOn: $isDanmakuEnabled) { Text("弹幕") }
+                }
+            }
+            .tag(2)
         }
+        .tabViewStyle(.page)
         .ignoresSafeArea()
         .accessibilityQuickAction(style: .prompt) {
             if isVideoPlayerGestureEnabled {
@@ -134,6 +142,56 @@ struct VideoPlayerView: View {
                     debugPrint(response)
                 }
             }
+            
+            AF.request("https://api.bilibili.com/x/v1/dm/list.so?oid=\(VideoDetailView.willPlayVideoCID)").response { response in
+                let danmakus = String(data: response.data!, encoding: .utf8)!
+                debugPrint(danmakus)
+                if danmakus.contains("<d p=\"") {
+                    let danmakuOnly = danmakus.split(separator: "</source>")[1].split(separator: "</i>")[0]
+                    let danmakuSpd = danmakuOnly.split(separator: "</d>")
+                    for singleDanmaku in danmakuSpd {
+                        let p = singleDanmaku.split(separator: "<d p=\"")[0].split(separator: "\"")[0]
+                        let spdP = p.split(separator: ",")
+                        var stredSpdP = [String]()
+                        for p in spdP {
+                            stredSpdP.append(String(p))
+                        }
+                        if singleDanmaku.split(separator: "\">").count < 2 {
+                            return
+                        }
+                        let danmakuText = String(singleDanmaku.split(separator: "\">")[1].split(separator: "</d>")[0])
+                        if stredSpdP[5] == "0" {
+                            showDanmakus.append(["Appear": stredSpdP[0], "Type": stredSpdP[1], "Size": stredSpdP[2], "Color": stredSpdP[3], "Text": danmakuText])
+                        }
+                    }
+                    showDanmakus.sort { dict1, dict2 in
+                        if let time1 = dict1["Appear"], let time2 = dict2["Appear"] {
+                            return Double(time1)! < Double(time2)!
+                        }
+                        return false
+                    }
+                    var removedCount = 0
+                    for i in 1..<showDanmakus.count {
+                        if showDanmakus.count - removedCount - i <= 0 {
+                            break
+                        }
+                        if (Double(showDanmakus[i]["Appear"]!)! - Double(showDanmakus[i - 1]["Appear"]!)!) < 0.5 {
+                            showDanmakus.remove(at: i)
+                            removedCount++
+                        }
+                    }
+                    debugPrint(showDanmakus)
+                }
+            }
+            
+            Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
+                danmakuTimer = timer
+                danmakuOffset = player.currentTime().seconds * 50
+            }
+        }
+        .onDisappear {
+            playerTimer?.invalidate()
+            danmakuTimer?.invalidate()
         }
     }
 }
