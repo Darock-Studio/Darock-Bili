79771221f18cf30a6821573e60220589c7b635b0
WindowsMEMZ <wmemz2333@163.com>
2024-02-24 15:42:10 +0800

    rev: Reverted dynamic link detect because of UI issue

--- MeowBili/Others/LinkDetectText.swift
+++ MeowBili/Others/LinkDetectText.swift
@@ -25,47 +25,53 @@ struct LinkDetectText: View {
     @State var markdownText = try! AttributedString(markdown: "")
     @State var linkColor = Color.blue
     @State var error: Error?
+    @State var isFinishedLoad = false
     var body: some View {
-        Group {
-            if let error {
-                VStack(alignment: .leading, content: {
-                    Text("文本加载失败")
-                    Text(error.localizedDescription)
-                        .font(.footnote)
-                })
-            } else {
-                Text(markdownText)
-                    .tint(linkColor)
-                #if os(watchOS)
-                //拦截，然后用暗礁浏览器打开
-                    .environment(\.openURL, OpenURLAction(handler: { url in
-                        //加一个小动画，因为下一步操作【真机上】会卡1秒
-                        bling()
-                        // Source: https://www.reddit.com/r/apple/comments/rcn2h7/comment/hnwr8do/
-                        let session = ASWebAuthenticationSession(
-                            url: url,
-                            callbackURLScheme: nil
-                        ) { _, _ in
-                        }
-                        
-                        // Makes the "Watch App Wants To Use example.com to Sign In" popup not show up
-                        session.prefersEphemeralWebBrowserSession = true
-                        
-                        session.start()
-                        return .handled
-                    }))
-                //iOS端可以直接用系统浏览器打开链接，不需要拦截
-                #endif
-            }
-        }
-        .onAppear {
-            loadText(inputURL: inputURL)
-        }
-        .onChange(of: inputURL, perform: { newValue in
-            loadText(inputURL: newValue)
-        })
-        .animation(.smooth, value: markdownText)
-     
+//        Group {
+//            if let error {
+//                VStack(alignment: .leading, content: {
+//                    Text("文本加载失败")
+//                    Text(error.localizedDescription)
+//                        .font(.footnote)
+//                })
+//            } else {
+//                Text(markdownText)
+//                    .tint(linkColor)
+//                #if os(watchOS)
+//                //拦截，然后用暗礁浏览器打开
+//                    .environment(\.openURL, OpenURLAction(handler: { url in
+//                        //加一个小动画，因为下一步操作【真机上】会卡1秒
+//                        bling()
+//                        // Source: https://www.reddit.com/r/apple/comments/rcn2h7/comment/hnwr8do/
+//                        let session = ASWebAuthenticationSession(
+//                            url: url,
+//                            callbackURLScheme: nil
+//                        ) { _, _ in
+//                        }
+//                        
+//                        // Makes the "Watch App Wants To Use example.com to Sign In" popup not show up
+//                        session.prefersEphemeralWebBrowserSession = true
+//                        
+//                        session.start()
+//                        return .handled
+//                    }))
+//                //iOS端可以直接用系统浏览器打开链接，不需要拦截
+//                #endif
+//            }
+//        }
+//        .onAppear {
+//            if !isFinishedLoad {
+//                loadText(inputURL: inputURL)
+//                isFinishedLoad = true
+//            }
+//        }
+//        .onChange(of: inputURL, perform: { newValue in
+//            loadText(inputURL: newValue)
+//        })
+//        .animation(.smooth, value: markdownText)
+        // FIXME: The view commented shows empty view when fast scrolling in the dynamic page.
+        // Tip: Reverted on February 24, 2024
+        Text(inputURL)
     }
     func loadText(inputURL: String) {
         let markdownURLs = detectURLs(in: inputURL)
@@ -88,19 +94,19 @@ struct LinkDetectText: View {
         })
     }
     func detectURLs(in text: String) -> String {
-            let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
-            let matches = detector?.matches(in: text, options: [], range: NSRange(location: 0, length: text.utf16.count))
-
-            var markdownString = text
-            if let matches = matches {
-                for match in matches {
-                    if let url = match.url {
-                        let link = "[\(url.absoluteString)](\(url.absoluteString))"
-                        markdownString = markdownString.replacingOccurrences(of: url.absoluteString, with: link)
-                    }
+        let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+        let matches = detector?.matches(in: text, options: [], range: NSRange(location: 0, length: text.utf16.count))
+        
+        var markdownString = text
+        if let matches = matches {
+            for match in matches {
+                if let url = match.url {
+                    let link = "[\(url.absoluteString)](\(url.absoluteString))"
+                    markdownString = markdownString.replacingOccurrences(of: url.absoluteString, with: link)
                 }
             }
-
-            return markdownString
         }
+        
+        return markdownString
+    }
 }
