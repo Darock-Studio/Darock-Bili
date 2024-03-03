2150409c379343846fed7fac4fb9955354e837fd
Mark Chan <45706356+WindowsMEMZ@users.noreply.github.com>
2024-03-03 21:33:53 +0800

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,7 +8,7 @@ then
         --method POST \
         -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        /repos/$CI_PULL_REQUEST_SOURCE_REPO/statuses/$CI_COMMIT \
+        /repos/Darock-Studio/Darock-Bili/statuses/$CI_COMMIT \
         -f state='pending' \
         -f description='XCloud Building...' \
         -f context='API Status Checker'
