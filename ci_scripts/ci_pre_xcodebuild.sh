dcce1df52c8410daf9fc5ab0d0a6b3250f02914b
WindowsMEMZ <wmemz2333@163.com>
2024-03-03 15:01:04 +0800

    ci: added codesign for dylib

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-if [ "$CI_WORKFLOW" = "Canary Deploy" ]; then
-    NEW_TEXT="Canary"
-elif [ "$CI_WORKFLOW" = "Dev Deploy" ]; then
-    NEW_TEXT="Dev"
-elif [ "$CI_WORKFLOW" = "Public Release" ]; then
-    NEW_TEXT="Stable"
-else
-    # 默认值，如果 CI_WORKFLOW 不匹配上述条件
-    NEW_TEXT="Unknown"
-fi
-
-sed -i '' "s|.*|${NEW_TEXT}|" ../CurrentChannel.drkdatac
-
 brew install swiftlint
