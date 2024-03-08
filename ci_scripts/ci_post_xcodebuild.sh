3a0fbebc9806c57e97009fbe6978b0f4bf924f87
WindowsMEMZ <wmemz2333@163.com>
2024-03-08 23:41:55 +0800

    Fix ci scripts

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -10,19 +10,3 @@ elif [[ "$CI_WORKFLOW" == "Public Release" ]]; then
   mkdir $TESTFLIGHT_DIR_PATH
   echo -e "当前语义化版本：$CI_TAG\n\nRelease Notes: https://github.com/Darock-Studio/Darock-Bili/releases/tag/$CI_TAG\n\n若要使用watchOS App，请打开“在Apple Watch上显示App”开关" > $TESTFLIGHT_DIR_PATH/WhatToTest.zh-Hans.txt
 fi
-
-if [[ -n $CI_PULL_REQUEST_NUMBER ]]; then
-    if [[ "$CI_XCODEBUILD_EXIT_CODE" == "0" ]]; then
-        NEW_STATUS=success
-    else
-        NEW_STATUS=failure
-    fi
-    
-    gh api \
-        --method POST \
-        -H "Accept: application/vnd.github+json" \
-        -H "X-GitHub-Api-Version: 2022-11-28" \
-        /repos/$CI_PULL_REQUEST_SOURCE_REPO/statuses/$CI_COMMIT \
-        -f state=$NEW_STATUS \
-        -f context='API Status Checker'
-fi
