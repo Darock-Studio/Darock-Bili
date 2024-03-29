576106005aa1921da7735d84fbec02ff9fff66fa
WindowsMEMZ <wmemz2333@163.com>
2024-03-29 20:30:25 +0800

    feat: Semantic Version for AboutView

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-if [ "$CI_WORKFLOW" = "Canary Deploy" ]; then
-    NEW_TEXT="Canary"
-elif [ "$CI_WORKFLOW" = "Dev Deploy" ]; then
-    NEW_TEXT="Dev"
-elif [ "$CI_WORKFLOW" = "Public Release" ]; then
-    NEW_TEXT="Stable"
+if [ "$CI_WORKFLOW" = "Public Release" ]; then
+    NEW_TEXT="Darock"
+elif [ "$CI_WORKFLOW" = "Linecom Release" ]; then
+    NEW_TEXT="Linecom"
 else
     # 默认值，如果 CI_WORKFLOW 不匹配上述条件
     NEW_TEXT="Unknown"
 fi
 
 sed -i '' "s|.*|${NEW_TEXT}|" ../CurrentChannel.drkdatac
+
+if [ -n $CI_TAG ]; then
+    sed -i '' "s|.*|${CI_TAG}|" ../SemanticVersion.drkdatas
+fi
 
 brew install swiftlint
