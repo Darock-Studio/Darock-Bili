1aea069a8c2aa4ff254f000a02ba65671ad67534
DannyFeng <FengzihangCode@outlook.com>
2024-12-22 16:40:37 +0800

    删除废弃文件并继续优化
    删除了废弃并导致编译出错的TrendingView.swift，图标化部分文字，重构了MeowWidget与BiliBiliAPIService

--- MeowWidget/MeowWidget.swift
+++ MeowWidget/MeowWidget.swift
@@ -30,7 +30,7 @@ struct Provider: TimelineProvider {
     }
 
     func getSnapshot(in context: Context, completion: @escaping (MeowWidgetEntry) -> Void) {
-        let placeholder = MeowWidgetEntry(date: Date(), video: Video(id: 0, title: "Snapshot", description: "Loading...", authorName: "Author", viewCount: 0, likeCount: 0, coinCount: 0, shareCount: 0, danmakuCount: 0))
+        let placeholder = MeowWidgetEntry(date: Date(), video: Video(id: 0, title: "miku miku oo ee o", description: "https://twitter.com/i/status/1697029186777706544 channel（twi:_CASTSTATION）", authorName: "未来de残像", viewCount: 0, likeCount: 0, coinCount: 0, shareCount: 0, danmakuCount: 0))
         completion(placeholder)
     }
 
@@ -52,29 +52,39 @@ struct MeowWidgetView: View {
     var entry: MeowWidgetEntry
 
     var body: some View {
+        let widgetURL = URL(string: "wget://openURL/\(entry.video.id)")
         switch family {
         case .accessoryInline:
-            Text("\(entry.video.title) ，作者： \(entry.video.authorName)")
+            Text(entry.video.title)
+                .widgetURL(widgetURL)
         case .accessoryCircular:
             VStack {
                 Image(systemName: "play.circle.fill")
+                    .foregroundColor(Color("WidgetTitleColor"))
                 Text(entry.video.title)
                     .font(.caption)
             }
+            .widgetURL(widgetURL)
         case .accessoryRectangular:
             VStack(alignment: .leading) {
                 Text(entry.video.title)
                     .font(.headline)
-                Text("作者： \(entry.video.authorName)")
+                Text(entry.video.authorName)
                     .font(.subheadline)
             }
+            .widgetURL(widgetURL)
         case .systemSmall:
             VStack {
                 Text(entry.video.title)
                     .font(.headline)
-                Text("作者： \(entry.video.authorName)")
-                    .font(.subheadline)
+                HStack {
+                    Image(systemName: "person.fill")
+                        .foregroundColor(Color("WidgetTitleColor"))
+                    Text(entry.video.authorName)
+                        .font(.caption)
+                }
             }
+            .widgetURL(widgetURL)
         case .systemMedium:
             HStack {
                 VStack(alignment: .leading) {
@@ -85,11 +95,22 @@ struct MeowWidgetView: View {
                         .lineLimit(2)
                 }
                 Spacer()
-                VStack {
-                    Text("观看量： \(entry.video.viewCount)")
-                    Text("点赞量： \(entry.video.likeCount)")
+                VStack(alignment: .leading) {
+                    HStack {
+                        Image(systemName: "play.rectangle")
+                            .foregroundColor(Color("WidgetTitleColor"))
+                        Text("\(entry.video.viewCount)")
+                            .font(.caption)
+                    }
+                    HStack {
+                        Image(systemName: "heart.fill")
+                            .foregroundColor(Color("WidgetTitleColor"))
+                        Text("\(entry.video.likeCount)")
+                            .font(.caption)
+                    }
                 }
             }
+            .widgetURL(widgetURL)
         case .systemLarge:
             VStack(alignment: .leading) {
                 Text(entry.video.title)
@@ -99,10 +120,26 @@ struct MeowWidgetView: View {
                     .lineLimit(3)
                 Spacer()
                 HStack {
-                    Text("观看量： \(entry.video.viewCount)")
-                    Text("点赞量： \(entry.video.likeCount)")
+                    HStack {
+                        Image(systemName: "play.rectangle.fill")
+                            .foregroundColor(Color("WidgetTitleColor"))
+                        Text("\(entry.video.viewCount)")
+                            .font(.footnote)
+                    }
+                    Spacer()
+                    HStack {
+                        Image(systemName: "heart.fill")
+                            .foregroundColor(Color("WidgetTitleColor"))
+                        Text("\(entry.video.likeCount)")
+                            .font(.footnote)
+                    }
                 }
+                Spacer()
+                Text("在喵哩喵哩查看视频")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
             }
+            .widgetURL(widgetURL)
         default:
             Text("Unsupported Widget Family")
         }
@@ -131,7 +168,9 @@ struct MeowWidget: Widget {
     }
 }
 
-/*The Old Version For Reference in Future Updates
+/*
+ 
+!   The older version for reference in few future updates, will be removed soon after more UI and function intergration
  
 import WidgetKit
 import SwiftUI
