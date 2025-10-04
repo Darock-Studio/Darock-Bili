a889dba30204a35a22015587db516bc4705edbf0
WindowsMEMZ <memz1@darock.top>
2025-10-05 01:40:12 +0800

    Update ci scripts

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -12,4 +12,8 @@ elif [[ "$CI_WORKFLOW" == "KentYe Alternative" ]]; then
   TESTFLIGHT_DIR_PATH=../TestFlight
   mkdir $TESTFLIGHT_DIR_PATH
   echo -e "由 Kent Ye 替代分发的喵哩喵哩\n\n当前语义化版本：$CI_TAG\n\nRelease Notes: https://github.com/Darock-Studio/Darock-Bili/releases/tag/$CI_TAG\n\n若要使用 watchOS App，请打开“在 Apple Watch 上显示 App”开关" > $TESTFLIGHT_DIR_PATH/WhatToTest.zh-Hans.txt
+elif [[ "$CI_WORKFLOW" == "Darock Alternative" ]]; then
+  TESTFLIGHT_DIR_PATH=../TestFlight
+  mkdir $TESTFLIGHT_DIR_PATH
+  echo -e "当前语义化版本：$CI_TAG\n\nRelease Notes: https://github.com/Darock-Studio/Darock-Bili/releases/tag/$CI_TAG\n\n若要使用 watchOS App，请打开“在 Apple Watch 上显示 App”开关" > $TESTFLIGHT_DIR_PATH/WhatToTest.zh-Hans.txt
 fi
--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -6,8 +6,9 @@ elif [ "$CI_WORKFLOW" = "Alternative" ]; then
     NEW_TEXT="785"
 elif [ "$CI_WORKFLOW" = "KentYe Alternative" ]; then
     NEW_TEXT="Kent Ye"
+elif [ "$CI_WORKFLOW" = "KentYe Alternative" ]; then
+    NEW_TEXT="Darock Alt"
 else
-    # 默认值，如果 CI_WORKFLOW 不匹配上述条件
     NEW_TEXT="Unknown"
 fi
 
